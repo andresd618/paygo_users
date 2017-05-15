2dee5ef208944c86fd6e6a91b023bf23d7c0d19a
Yeferson Andres Hernandez Daza <andreshernandez@DATECSA.LAN>
2017-05-15 13:19:54 -0500

    Se crean componentes y servicios a utilizar

--- paygousersfront/src/app/app.module.ts
+++ paygousersfront/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/users/list/list.component';
+import { UploadComponent } from './components/users/upload/upload.component';
+import { RecordComponent } from './components/users/record/record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    UploadComponent,
+    RecordComponent
   ],
   imports: [
     BrowserModule,
