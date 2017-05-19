13620e6431786d2ec6ca4a58861658a421fb1986
Yeferson Andres Hernandez Daza <andreshernandez@DATECSA.LAN>
2017-05-18 23:55:32 -0500

    Se deshabilita el boton subir mientras se cargan los usuarios de un archivo csv

--- paygousersfront/src/app/components/users/upload/upload.component.ts
+++ paygousersfront/src/app/components/users/upload/upload.component.ts
@@ -16,12 +16,14 @@ export class UploadComponent implements OnInit {
   warningMsg: String = "";
   file : File;
   formUploadUsers : FormGroup;
+  processing : boolean;   ///Usado para habilitar/deshabilitar el boton subir
 
   @ViewChild('inpFileUpload')
   inpFileUpload: any;
 
   constructor(private _api : ApirestService) {
      this.formUploadUsers = new FormGroup({});
+     this.processing = false;
    }
 
    ngOnInit(){}
@@ -48,10 +50,12 @@ export class UploadComponent implements OnInit {
    * Realiza la importacion de usuarios desde el archivo CSV
    */
   uploadUsers(){
-
+      
       this._api.uploadUsers(this.file).subscribe(
 
           (res) => {
+              this.processing = false; ///Se habilita el boton subir
+
               if(res.status){
                   this.sucessMsg = res.msg;
               }else{
@@ -62,11 +66,12 @@ export class UploadComponent implements OnInit {
               this.inpFileUpload.nativeElement.value = "";
           },
           (error) => {
-            
+            this.processing = false;
             this.errors = error.json().msg;            
           }
       );
   }
+
 
 
   /**
@@ -79,6 +84,8 @@ export class UploadComponent implements OnInit {
 
           this.cleanMsgs();
 
+          this.processing = true; //Se deshabilita el boton subir
+
           ///Primero se eliminan los usuarios de la bd (truncate)
           this._api.deleteAllUsers().subscribe(
               (res) => {
@@ -86,10 +93,12 @@ export class UploadComponent implements OnInit {
                     ///Si se eliminan los usuarios se cargan los nuevos
                     this.uploadUsers();
                   }else{
+                    this.processing = false; 
                     this.warningMsg = res.msg;
                   }
               },
               (error) => {
+                this.processing = false;
                 this.errors = error.json().msg;                
               }            
           );          
